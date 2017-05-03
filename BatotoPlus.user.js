a5c24958b52ef7e6ed2b70d214a103382a132c6e
Justin <egliju@onid.orst.edu>
2017-05-03 13:28:32 -0700

    Version Update

--- BatotoPlus.user.js
+++ BatotoPlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             JustJustin.BatotoPlus
 // @name           Batoto Plus
-// @version        1.3.7
+// @version        1.3.8
 // @namespace      JustJustin
 // @author         JustJustin
 // @description    Adds new features to Batoto
